f584c0dabd174e58bda8a8558ef555f0e1ecd9d1
ajvergara <ajvergara1014@gmail.com>
2018-05-10 14:55:31 +0300

    Completed Large Hero Destop & Mobile Version

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,13 @@ const gulp         = require("gulp"),
       autoprefixer = require("autoprefixer"),
       cssvars      = require("postcss-simple-vars"),
       cssimport    = require("postcss-import"),
-      cssnested    = require("postcss-nested")
-      browserSync   = require("browser-sync").create();
+      cssnested    = require("postcss-nested"),
+      cssmixin     = require("postcss-mixins"),
+      browserSync  = require("browser-sync").create();
 
 gulp.task("styles", function(){
   return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssvars, cssimport, autoprefixer, cssnested]))
+    .pipe(postcss([cssvars, cssimport, cssmixin, autoprefixer, cssnested]))
     .on("error", function(errorInfo){
       console.log(errorInfo.toString());
       this.emit("end");
