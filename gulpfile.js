e362ce5d5aa809fa58ec49a948b4a5afd720214d
ajvergara <ajvergara1014@gmail.com>
2018-05-14 15:59:43 +0300

    Completed Desktop Version of Testimonials

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const gulp         = require("gulp"),
 
 gulp.task("styles", function(){
   return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssvars, cssimport, cssmixin, autoprefixer, cssnested]))
+    .pipe(postcss([cssimport, cssvars, cssmixin, autoprefixer, cssnested]))
     .on("error", function(errorInfo){
       console.log(errorInfo.toString());
       this.emit("end");
