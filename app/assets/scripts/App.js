718e34feffdbce9b323cb2ddd1992e8aa3e1e3fb
ajvergara <ajvergara1014@gmail.com>
2018-05-24 13:47:05 +0300

    Completed JS Webpack & Babel dependencies - continue header mobile icon functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,15 @@
-var Person = require("./modules/Person");
+import Person from "./modules/Person";
+
+class Adult extends Person{
+  payTax(){
+    console.log(this.name + " pays $23 in taxes.");
+  }
+}
 
 var joe = new Person("Joe", "blue");
 
 joe.greet();
 
-var jane = new Person("Jane", "pink");
-
+var jane = new Adult("Jane", "pink");
 jane.greet();
+jane.payTax();
--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,9 +1,12 @@
-function Person(fullName, favColor){
+
+class Person{
+  constructor(fullName, favColor){
     this.name = fullName;
     this.color = favColor;
     this.greet = function(){
       console.log("Hi! my name is " + this.name + " and my favorite color is " + this.color);
     }
+  }
 }
 
-module.exports = Person;
+export default Person;
