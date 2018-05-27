38f47c5e3434b2075f67f002afbbdb1339f08ee6
ajvergara <ajvergara1014@gmail.com>
2018-05-27 14:31:50 +0300

    Completed github pages preparation - ready for deplaoyment

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -8,19 +8,19 @@ const gulp     = require("gulp"),
       rev      = require("gulp-rev"),
       browserSync  = require("browser-sync").create();
 
-gulp.task("previewDist", function(){
+gulp.task("previewDocs", function(){
   browserSync.init({
     server: {
-      baseDir: "dist"
+      baseDir: "docs"
     }
   });
 });
 
-gulp.task("deleteDist", ["icons"], function(){
-  return del("./dist");
+gulp.task("deleteDocs", ["icons"], function(){
+  return del("./docs");
 });
 
-gulp.task("generalFiles", ["deleteDist"], function(){
+gulp.task("generalFiles", ["deleteDocs"], function(){
   let pathsToCopy = [
     "./app/**/*",
     "!./app/*.html",
@@ -32,10 +32,10 @@ gulp.task("generalFiles", ["deleteDist"], function(){
     "!./app/temp/**/*"
   ]
   return gulp.src(pathsToCopy)
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./docs"));
 });
 
-gulp.task("optimizeImages", ["deleteDist"], function(){
+gulp.task("optimizeImages", ["deleteDocs"], function(){
   return gulp.src(["./app/assets/images/**/*", "!./app/assets/images/icons", "!./app/assets/images/icons/**/*"])
     .pipe(imagemin([
       imagemin.gifsicle({interlaced: true}),
@@ -48,10 +48,10 @@ gulp.task("optimizeImages", ["deleteDist"], function(){
         ]
       })
     ]))
-    .pipe(gulp.dest("./dist/assets/images"));
+    .pipe(gulp.dest("./docs/assets/images"));
 });
 
-gulp.task("useminTrigger", ["deleteDist"], function(){
+gulp.task("useminTrigger", ["deleteDocs"], function(){
   gulp.start("usemin");
 });
 
@@ -62,7 +62,7 @@ gulp.task("usemin", ["styles", "scripts"], function(){
       html: [ function(){ return htmlmin({ collapseWhitespace: true}) }],
       js: [function(){ return uglify() }, function(){ return rev() }]
     }))
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./docs"));
 });
 
-gulp.task("build", ["deleteDist", "generalFiles", "optimizeImages", "useminTrigger"]);
+gulp.task("build", ["deleteDocs", "generalFiles", "optimizeImages", "useminTrigger"]);
