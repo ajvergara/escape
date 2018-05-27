a0e71c7e5938c08da259f8dcecc1deeb08c42cdf
ajvergara <ajvergara1014@gmail.com>
2018-05-27 11:36:47 +0300

    Completed navigation link appearance

--- app/assets/scripts/modules/Header.js
+++ app/assets/scripts/modules/Header.js
@@ -6,6 +6,12 @@ class Header{
     this.siteHeader = $(".site-header");
     this.headerTrigger = $(".large-hero__title");
     this.headerWaypoints();
+    this.pageSection = $(".page-section");
+    this.navWaypoints();
+    this.navlinks = $(".primary-nav a");
+    this.homeTag = $("#escape-link");
+    this.homeTagTrigger = $("#escape");
+    this.homeWaypoints();
   }
 
   headerWaypoints(){
@@ -14,6 +20,56 @@ class Header{
       element: this.headerTrigger[0],
       handler: function(){
         that.siteHeader.toggleClass("site-header--dark");
+      }
+    });
+  }
+
+  navWaypoints(){
+    var that = this;
+    this.pageSection.each(function(){
+      var currentPage = this;
+      new Waypoint({
+        element: currentPage,
+        handler: function(direction){
+          if(direction == "down"){
+            var dataMatchLink = currentPage.getAttribute("data-link");
+            that.navlinks.removeClass("is-current-link");
+            $(dataMatchLink).addClass("is-current-link");
+          }
+        },
+        offset: "80%"
+      });
+
+      new Waypoint({
+        element: currentPage,
+        handler: function(direction){
+          if(direction == "up"){
+            var dataMatchLink = currentPage.getAttribute("data-link");
+            that.navlinks.removeClass("is-current-link");
+            $(dataMatchLink).addClass("is-current-link");
+          }
+        },
+        offset: "-40%"
+      });
+    });
+  }
+
+  homeWaypoints(){
+    var that = this;
+    new Waypoint({
+      element: this.homeTagTrigger[0],
+      handler: function(){
+        that.homeTag.addClass("is-current-link");
+      }
+    });
+
+    new Waypoint({
+      element: this.headerTrigger[0],
+      handler: function(direction){
+        if(direction == "up"){
+          that.navlinks.removeClass("is-current-link");
+          that.homeTag.addClass("is-current-link");
+        }
       }
     });
   }
